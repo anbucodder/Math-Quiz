e732c5867a060fc162b80ef871406acb0d8be99d
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-04-06 20:12:15 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -4,8 +4,8 @@ player2_name=localStorage.getItem("player2_name");
 player1_score = "0";
 player2_score = "0";
 
-document.getElementById("player1_name").innerHTML=player1_name+" : ";
-document.getElementById("player2_name").innerHTML=player2_name+" : ";
+document.getElementById("player1_name").innerHTML=player1_name + " : ";
+document.getElementById("player2_name").innerHTML=player2_name + " : ";
 
 document.getElementById("player1_score").innerHTML=player1_score;
 document.getElementById("player2_score").innerHTML=player2_score;
@@ -13,8 +13,8 @@ document.getElementById("player2_score").innerHTML=player2_score;
 question_turn="player1";
 answer_turn="player2";
 
-document.getElementById("player_question").innerHTML="Question turn - "+player1_name;
-document.getElementById("player_answer").innerHTML="Answer turn - "+player2_name;
+document.getElementById("player_question").innerHTML="Question turn - " + player1_name;
+document.getElementById("player_answer").innerHTML="Answer turn - " + player2_name;
 
 function send(){
     num1=document.getElementById("num1").value;
@@ -22,13 +22,11 @@ function send(){
     actual_answer=parseInt(num1)*parseInt(num2);
     console.log("number1 = "+num1+" | number2 = "+num2);
     console.log(actual_answer);
-    question=num1+" x "+num2;
-    console.log(question);
-
-    question_word="<h4 id='word_display'>Q."+question+"</h4>";
+    
+    question="<h4>"+num1+" x "+num2+"</h4>";
     text_input="<br> Answer = <input id='input_check_box' type='text'></input>";
     check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row = question_word + text_input + check_button;
+    row = question + text_input + check_button;
     document.getElementById("output").innerHTML=row;
     document.getElementById("num1").innerHTML="";
     document.getElementById("num2").innerHTML="";
